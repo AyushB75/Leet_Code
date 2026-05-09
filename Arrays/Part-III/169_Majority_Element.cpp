d74546666d1a08dfb0086472e920f9618dd2187e
AyushB75 <20ayushbansal@gmail.com>
2026-05-09 17:58:30 +0530

    Add Arrays Part-III - 169 Majority Element

---
+++ Arrays/Part-III/169_Majority_Element.cpp
@@ -0,0 +1,35 @@
+// 169. Majority Element
+// Difficulty: Easy | Topic: Arrays Part-III | Language: C++
+// https://leetcode.com/problems/majority-element/
+//
+// Problem: Given an array nums of size n, return the element that appears
+// more than n/2 times. The majority element always exists in the array.
+//
+// Approach: Boyer-Moore Voting Algorithm. Maintain a candidate and a count.
+// If count is 0, update candidate. If current element matches candidate
+// increment count, else decrement. The final candidate is the majority element.
+// Time: O(n) | Space: O(1)
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, count = 0;
+        for (int num : nums)
+        {
+            if (count == 0)
+            {
+                candidate = num;
+            }
+            if (num == candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
